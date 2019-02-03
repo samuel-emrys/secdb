d4f80968c8fbf93c04a03990b5a888d237e91e57
Samuel Dowling <samuel.dowling@protonmail.com>
2019-02-03 23:40:58 +1030

    Added package information

--- setup.py
+++ setup.py
@@ -1,13 +1,83 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from __future__ import absolute_import
+from __future__ import print_function
 
-setup(name='SecDB',
-      version='1.0',
-      description='Securities Database Maintenance Tool',
-      author='Samuel Dowling',
-      author_email='samuel.dowling@protonmail.com',
-      url='https://github.com/samuel-emrys/secdb',
-      packages=find_packages(),
-      license="GPLv3"
-     )
+import io
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='SecDB',
+    version='0.1.0',
+    license='GPLv3',
+    description='Securities Database Maintenance Tool',
+    long_description="secdb is a financial market data \
+    aggregation program. It aggregates data from a number \
+    of websites and freely available APIs to build and \
+    maintain a SQL database. This database can then be \
+    used as a source for financial market analysis with \
+    confidence.",
+    author='Samuel Dowling',
+    author_email='samuel.dowling@protonmail.com',
+    url='https://github.com/samuel-emrys/secdb',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: POSIX :: BSD :: FreeBSD',
+        'Operating System :: MacOS',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Utilities',
+    ],
+    keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
+    ],
+    install_requires=[
+        # 'click',
+        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+    ],
+    extras_require={
+        # eg:
+        #   'rst': ['docutils>=0.11'],
+        #   ':python_version=="2.6"': ['argparse'],
+    },
+    entry_points={
+        'console_scripts': [
+            'secdb = secdb.application:main',
+        ]
+    },
+)
