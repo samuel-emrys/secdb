47a1a29b3f2aa038d780920efbefc99fdda3fa1b
Samuel Dowling <samuel.dowling@protonmail.com>
2019-02-18 22:55:10 +1030

    Implemented tests for aggregator functions

---
+++ tests/test_aggregator.py
@@ -0,0 +1,35 @@
+from secdb.aggregator import Aggregator
+from secdb.application import import_vendors
+import secdb.database
+
+
+session = secdb.database.connect()
+agg = Aggregator(session)
+
+
+def test_vendor_add():
+    vendors = import_vendors()
+
+    agg.import_vendors(vendors)
+
+
+def test_currency_add():
+    currencies = []
+    for vendor in agg.vendors:
+        currencies.append(vendor.build_currency())
+    agg.import_currencies(currencies)
+
+
+def test_exchange_add():
+    exchanges = []
+    for vendor in agg.vendors:
+        exchanges.append(vendor.build_exchanges())
+    agg.import_exchanges(exchanges)
+
+
+def test_symbol_add():
+    pass
+
+
+def test_price_add():
+    pass
