82c2f6cae85f33654f2e8e4d49dd2b04990022f6
Samuel Dowling <samuel.dowling@protonmail.com>
2019-02-26 21:42:42 +1100

    Added unit tests for World Trading Data vendor

---
+++ tests/test_worldtradingdata.py
@@ -0,0 +1,26 @@
+from secdb.vendors.worldtradingdata import WorldTradingData
+from secdb.application import import_vendors
+
+vendors = import_vendors()
+
+for vendor in vendors:
+    if vendor.name == 'World Trading Data':
+        wtd = vendor
+    elif vendor.name == 'Currency-ISO':
+        ciso = vendor
+
+
+def test_build_exchanges():
+    wtd.build_exchanges()
+
+    print ("Number of exchanges: " + str(len(wtd.exchanges)))
+    assert len(wtd.exchanges) == 40
+
+
+def test_build_symbols():
+    currencies = ciso.build_currency()
+
+    wtd.build_symbols(currencies, wtd.exchanges)
+    print("Symbol array length: " + str(len(wtd.symbols)))
+
+    assert len(wtd.symbols) > 0
